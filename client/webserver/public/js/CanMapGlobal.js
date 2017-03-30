b0fe94952a6449fc7ec26100f67cac39ce9dc0d9
Cory Welch <cory.welch77@gmail.com>
2017-03-30 02:05:24 -0400

    Recreating dynamicURL.js

--- client/webserver/public/js/CanMapGlobal.js
+++ client/webserver/public/js/CanMapGlobal.js
@@ -15,9 +15,21 @@
  * Should be included by all html files
  * 
  */
+ 
+// isInvalid will return true if undefined or type is undefined
+function isInvalid(item){
+    if( item == undefined || item == "undefined" || typeof(item) == undefined || typeof(item) == "undefined" ){
+        return true;
+    } else {
+        return false;
+    }
+}
 
+//Additional Date Prototype to easy add days to a date.
 Date.prototype.addDays = function(days) { //http://stackoverflow.com/questions/563406/add-days-to-javascript-date
   var dat = new Date(this.valueOf());
   dat.setDate(dat.getDate() + days);
   return dat;
 }
+
+
