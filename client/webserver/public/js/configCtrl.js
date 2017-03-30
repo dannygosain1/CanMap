461e67013999ff3f878f6a65af081d407a783e75
Cory Welch <cory.welch77@gmail.com>
2017-03-30 19:00:18 -0400

    Pre Rebase Commit

--- client/webserver/public/js/configCtrl.js
+++ client/webserver/public/js/configCtrl.js
@@ -1,6 +1,3 @@
-
-<<<<<<< HEAD
-<<<<<<< HEAD
 apiURL = "";
 
 app.controller("configCtrl", function($scope, $http) {
@@ -10,19 +7,4 @@ app.controller("configCtrl", function($scope, $http) {
         });
     }
 });
-=======
-app.controller("configCtrl", function($scope, $http) {
-    
-}
->>>>>>> Adding Controllers
-=======
-apiURL = "";
 
-app.controller("configCtrl", function($scope, $http) {
-    $scope.getApiURL = function(){
-        $http.get('/config/apiConfig.json').then(function(result){
-            apiURL = result.apiURL;
-        });
-    }
-});
->>>>>>> fixing controller issue, updating api url
--- client/webserver/public/js/renderCtrl.js
+++ client/webserver/public/js/renderCtrl.js
@@ -1,31 +1,17 @@
 
-app.controller("renderCtrl", function($scope, $http) {
+app.controller("renderCtrl", function($scope, $rootScope, $http) {
 
     $scope.renderMap = function() {
-<<<<<<< HEAD
-<<<<<<< HEAD
-        return $http.get("http://7ca07d79.ngrok.io/canada/"+$scope.datasetName+"/sum")
-=======
-        return $http.get("http://a249d577.ngrok.io/canada/"+$scope.datasetName+"/sum")
->>>>>>> Adding Controllers
-=======
-        return $http.get("http://50a90602.ngrok.io/canada/"+$scope.datasetName+"/sum")
->>>>>>> fixing controller issue, updating api url
+        return $http.get($rootScope.apiURL+"/canada/"+$scope.datasetName+"/sum")
             .then(function(response) {
                 var data = response.data;
                 $http.get('/maps/canada.geo.json')
                     .then(function(result) {
-<<<<<<< HEAD
                         $("#spinner").hide();
                         $('#container').show();
                         Highcharts.mapChart('container', {
                             title: {
                                 text: $scope.selectedDataset
-=======
-                        Highcharts.mapChart('container', {
-                            title: {
-                                text: 'Sample Title'
->>>>>>> Adding Controllers
                             },
                             mapNavigation: {
                                 enabled: true,
@@ -34,33 +20,22 @@ app.controller("renderCtrl", function($scope, $http) {
                                 }
                             },
                             colorAxis: {
-<<<<<<< HEAD
                                 tickPixelInterval: 100,
                                 minColor:'#ffd1d1',
                                 maxColor:'#8c0202',
-=======
-                                tickPixelInterval: 100
->>>>>>> Adding Controllers
                             },
                             series: [{
                                 name: $scope.selectedDataset,
                                 data: data,
                                 mapData: result.data,
-<<<<<<< HEAD
                                 borderColor: "#fff",
                                 borderWidth: 1,
-=======
->>>>>>> Adding Controllers
                             }],
                             credits: {
                                 enabled: false
                             },
                             chart: {
-<<<<<<< HEAD
                                 backgroundColor:'rgba(255, 255, 255, 0.1)',
-=======
-                                backgroundColor:'rgba(255, 255, 255, 0.1)'
->>>>>>> Adding Controllers
                             },
                             exporting: {
                                 enabled: false
@@ -69,30 +44,14 @@ app.controller("renderCtrl", function($scope, $http) {
                     })
             })
     }
-<<<<<<< HEAD
-<<<<<<< HEAD
-    $http.get("http://7ca07d79.ngrok.io/datasets")
-    .then(function(response){
-        $scope.datasets = response.data;
-        $scope.selectedDataset = $scope.datasets[0];
-        $("#spinner").show();
-        $scope.datasetName = $scope.datasets[0].replace(/ /g,"_");
-    }).then($scope.renderMap);
-    $scope.update = function() {
-        $('#container').hide();
-        $("#spinner").show();
-=======
-    $http.get("http://a249d577.ngrok.io/datasets")
-=======
-    $http.get("http://50a90602.ngrok.io/datasets")
->>>>>>> fixing controller issue, updating api url
+
+    $http.get($rootScope.apiURL+"/datasets")
     .then(function(response){
         $scope.datasets = response.data;
         $scope.selectedDataset = $scope.datasets[0];
         $scope.datasetName = $scope.datasets[0].replace(/ /g,"_");
     }).then($scope.renderMap);
     $scope.update = function() {
->>>>>>> Adding Controllers
         $scope.datasetName = $scope.selectedDataset.replace(/ /g,"_");
         $scope.renderMap();
     };
