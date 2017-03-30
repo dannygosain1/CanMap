d414c1f7f84bdef572d582f0127a294e6109e0bb
dannygosain1 <dgosain@uwaterloo.ca>
2017-03-30 00:55:46 -0400

    Added dropdown for datasets

--- client/webserver/public/js/app.js
+++ client/webserver/public/js/app.js
@@ -1,5 +1,13 @@
-var app = angular.module("dropdownList", []);
-console.log("in app.js")
+var app = angular.module("canMap", []);
 app.controller("myCtrl", function($scope) {
-    $scope.products = ["Dataset1", "Dataset2", "Dataset3"];
+    $scope.datasets = ["Dataset1", "Dataset2", "Dataset3"];
 });
+
+// <script>
+// var app = angular.module('myApp', []);
+// app.controller('myCtrl', function($scope) {
+//     $scope.names = ["Emil", "Tobias", "Linus"];
+// });
+// </script>
+//
+// <p>This example shows how to fill a dropdown list using the ng-options directive.</p>
