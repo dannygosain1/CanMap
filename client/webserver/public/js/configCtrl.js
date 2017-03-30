3acf4ebb9eaf708261ec92124672f42fd9d6411e
Cory Welch <cory.welch77@gmail.com>
2017-03-30 19:00:18 -0400

    Pre Rebase Commit

--- client/webserver/public/js/configCtrl.js
+++ client/webserver/public/js/configCtrl.js
@@ -1,10 +1,20 @@
 
-apiURL = "";
-
-app.controller("configCtrl", function($scope, $http) {
+app.controller("configCtrl", function($scope, $rootScope, $http) {
+    
     $scope.getApiURL = function(){
         $http.get('/config/apiConfig.json').then(function(result){
-            apiURL = result.apiURL;
+            $rootScope.apiURL = result.data.apiURL;
         });
     }
+    
+    $scope.getProvIdMapping = function(){
+        $http.get('/config/provinceIdMapping.json').then(function(result){
+            $rootScope.provIdMapping = result.data.data;
+        });
+    }
+    
+    $scope.getApiURL();
+    $scope.getProvIdMapping();
+    
+    
 });
