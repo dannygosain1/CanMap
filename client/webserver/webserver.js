01c39cb126cefb805d6ad4116fac1efeca9f4474
dannygosain1 <dgosain@uwaterloo.ca>
2017-03-30 19:11:52 -0400

    fixed merge conflict

--- client/webserver/webserver.js
+++ client/webserver/webserver.js
@@ -74,7 +74,7 @@ root.get('/config/:file', function(req, res){
         if(err){
             res.send(err);
         }
-        console.log(data);
+        //console.log(data);
         res.json(JSON.parse(data));
     });
 });
