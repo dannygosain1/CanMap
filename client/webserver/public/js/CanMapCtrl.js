7f0ddc2404816cd5278780a5f11aa625912d170d
Cory Welch <cory.welch77@gmail.com>
2017-03-31 04:32:56 -0400

    removing console log message

--- client/webserver/public/js/CanMapCtrl.js
+++ client/webserver/public/js/CanMapCtrl.js
@@ -33,7 +33,6 @@ app.controller("canMapCtrl", function($scope, $rootScope, $http) {
             if(key == 'p'){
 
                 if(isNaN(val) || val < 0 || isInvalid($scope.provIdMapping[val])){
-                    console.log('Invalid ProvID='+val);
                     return; // = continue for forEach
                 }
 
