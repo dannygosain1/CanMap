900e54944c72d35d1e568a4e05a747848ce09ae3
dannygosain1 <dgosain@uwaterloo.ca>
2017-03-30 22:00:23 -0400

    Fixed spinner on url change

--- client/webserver/public/js/CanMapCtrl.js
+++ client/webserver/public/js/CanMapCtrl.js
@@ -1,23 +1,23 @@
 
 app.controller("canMapCtrl", function($scope, $rootScope, $http) {
-   
+
    // CONFIG FUNCTIONS
    $scope.getApiURL = function(){
        return $http.get('/config/apiConfig.json').then(function(result){
             $scope.apiURL = result.data.apiURL;
         });
     }
-    
+
     $scope.getProvIdMapping = function(){
         return $http.get('/config/provinceIdMapping.json').then(function(result){
             $scope.provIdMapping = result.data.data;
         });
     }
-    
+
     $scope.loadConfig = function(){
         return $scope.getApiURL().then($scope.getProvIdMapping);
     }
-    
+
     //PARAMETER MANIPULATION FUNCTIONS
     //jquery
     $scope.paramsFromURL = function(){
@@ -28,38 +28,38 @@ app.controller("canMapCtrl", function($scope, $rootScope, $http) {
             var keyval = param.split('=');
             var key = keyval[0].toLowerCase();
             var val = keyval[1];
-            
+
             if(key == 'p'){
-                
+
                 if(isNaN(val) || val < 0){
                     console.log('Invalid ProvID='+val);
                     return; // = continue for forEach
                 }
-                
+
                 $scope.url_params.p = val;
-                
+
             } else if(key == 'd'){
                 if(isNaN(val) || val < 0 || val > $scope.datasets.length){
                     console.log('Invalid Dataset ID='+val);
                     return; // = continue for forEach
                 }
-                
+
                 $scope.url_params.d = val.replace(" ","_");
-                
+
             }
         });
-        
+
         if(isInvalid($scope.url_params.d)){
             $scope.url_params.d = $scope.defaultSelectedDatasetId;
         }
-        
+
         if(isInvalid($scope.url_params.p)){
             $scope.url_params.p = 1;
         }
-        
+
         return;
     }
-    
+
     //jquery
     $scope.updateURL = function(path){
 
@@ -68,21 +68,20 @@ app.controller("canMapCtrl", function($scope, $rootScope, $http) {
             var pathname = $(location).attr('pathname');
             path = pathname + '?' + $.param($scope.url_params);
         }
-        
+
         history.pushState({},'UpdatedURL',path);
         return $scope.renderPage();
     }
-     
+
     $scope.loadPage = function(){
         $scope.paramsFromURL()
         return $scope.renderPage();
-        
-    } 
-    
+    }
+
     //GET DATASET LIST
     $scope.getDatasetList = function(){
         return $http.get($scope.apiURL+"/datasets").then(function(results){
-            
+
             $scope.datasets = [];
             for(var d = 0; d < results.data.length; d++){
                 $scope.datasets.push({
@@ -94,10 +93,10 @@ app.controller("canMapCtrl", function($scope, $rootScope, $http) {
             //Manipulate Dataset list if necessary here, for now start with pop 2016 = [1]
             $scope.defaultSelectedDatasetId = 1;
             $scope.selectedDataset = $scope.datasets[$scope.defaultSelectedDatasetId]; //OBJ!
-            
+
         });
     }
-    
+
     //RENDER FUNCTIONS
     $scope.renderPage = function(){
         //Logic based on parameters
@@ -147,27 +146,29 @@ app.controller("canMapCtrl", function($scope, $rootScope, $http) {
             })
 
     }
-    
+
     // DATA SELECT UPDATED
     $scope.datasetUpdate = function() {
-        
+
         $scope.url_params.d = $scope.selectedDataset.id;
         $scope.selectedDatasetId = $scope.selectedDataset.id;
+        $('#container').hide();
+        $("#spinner").show();
         return $scope.updateURL();
     };
-    
-    
+
+
     // EXECUTE ON LOAD
     $scope.datasets = [];
     $scope.url_params = {};
     $scope.loadConfig().then($scope.getDatasetList).then($scope.loadPage);
-    
-    
+
+
     // need to switch to angular directive
     window.addEventListener('popstate', function(event) {
         //console.log('window.addEventListener(popstate) Called');
         return $scope.loadPage();
     });
-    
-   
+
+
 });
