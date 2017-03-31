4f6829fd0f7e6e94cf100742db4d866a36281997
RaunaqSuri <raunaq.suri@hotmail.com>
2017-03-31 01:30:39 -0400

    added citation

--- client/webserver/public/js/CanMapUtils.js
+++ client/webserver/public/js/CanMapUtils.js
@@ -24,7 +24,7 @@ function isInvalid(item){
         return false;
     }
 }
-
+//http://stackoverflow.com/questions/1144783/how-to-replace-all-occurrences-of-a-string-in-javascript
 String.prototype.replaceAll = function(old, replacement) {
 	return this.split(old).join(replacement);
 }
